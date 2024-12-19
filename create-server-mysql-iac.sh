bd2dbff5d3585bb4a24a0c725771cf5304a110ab
Thiago Jarilho Zardo <thiago.artevisual@gmail.com>
2024-12-18 21:57:34 -0300

    feat #01 (script inicial)

---
+++ create-server-mysql-iac.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+echo "Atualizando server..."
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando dependencias..."
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando repositorio..."
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+
+echo "Descompactando..."
+unzip main.zip
+
+echo "Copiando arquivos..."
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+echo "Habilitando permissões..."
+chmod +x create-server-mysql-iac.sh
